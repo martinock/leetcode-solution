6dd34dd3669c9ccd2fbdcb75c25f3532a4708a8c
Martino Christanto <martino.aksel.11@gmail.com>
2020-08-20 19:02:06 +0700

    feat(easy): twoSum solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # leetcode-solution
-My solution on LeetCode problems
+My solution on LeetCode problems written in Golang
 
 Feel free to give feedbacks on my solutions. All feedbacks are really appreciated :)
---
+++ easy/two-sum.go
@@ -0,0 +1,18 @@
+package easy
+
+// twoSum search two numbers in array that sums is equal to target, then return the indices
+// e.g. nums = [2, 7, 11, 15]; target = 9
+// nums[0] + nums[1] = 2 + 7 = 9 -> return [0, 1]
+// Problems: https://leetcode.com/problems/two-sum/submissions/
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	// default case that is mandatory in Golang
+	return []int{}
+}
