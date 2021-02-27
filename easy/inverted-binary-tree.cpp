f91907ca98fd8b21603844af8704986fb7e7ce44
Martino Christanto <martino.aksel.11@gmail.com>
2021-02-27 11:02:51 +0700

    add balanced binary tree draft

--- easy/inverted-binary-tree.cpp
+++ easy/inverted-binary-tree.cpp
@@ -27,3 +27,19 @@ public:
         return inverted;
     }
 };
+
+// BETTER SOLUTION(?):
+// class Solution {
+// public:
+//     TreeNode* invertTree(TreeNode* root) {
+//         if (root == NULL) {
+//             return NULL;
+//         }
+//         TreeNode* left = invertTree(root->left);
+//         TreeNode* right = invertTree(root->right);
+//         root->left = right;
+//         root->right = left;
+
+//         return root;
+//     }
+// };
