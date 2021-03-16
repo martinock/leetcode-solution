7e11f342fc63ae91e446a23ae9ae6d8c12c02080
Martino Christanto <martino.aksel.11@gmail.com>
2021-03-16 19:39:48 +0700

    chore: add better solution

--- easy/middle-of-linked-list.cpp
+++ easy/middle-of-linked-list.cpp
@@ -36,3 +36,13 @@ public:
         return currHalf;
     }
 };
+
+// BETTER SOLUTION:
+// func middleNode(head *ListNode) *ListNode {
+//     slow, fast := head, head 
+//     for fast != nil && fast.Next != nil {
+//         slow = slow.Next
+//         fast = fast.Next.Next
+//     }
+//     return slow
+// }
